3fe0e0678ac6209bc28df6aaf5634ad995efaef3
Imorana <nvi22724@gmail.com>
2023-03-15 12:58:16 +1300

    Added in a question

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
 print("Kia ora you know who it is leave a message and i will get back to you")
-question_one = input("How any legs does a cheetah have")
+
 options = ["1", "2", "3", "4"]
+question_one = input("How many legs does a cheetah have")
+#print(check(options, question_one))
